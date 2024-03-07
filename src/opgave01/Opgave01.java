eb91162c7f9542b7588aa1d8be63393157f5b4c4
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-03-07 10:26:16 +0100

    finished exercise 1

--- src/opgave01/Opgave01.java
+++ src/opgave01/Opgave01.java
@@ -7,5 +7,20 @@ import java.util.Set;
 public class Opgave01 {
     public static void main(String[] args) {
         Set<Integer> mySet = new HashSet<>(List.of(34, 12, 23, 45, 67, 34, 98 ));
+        printHashSet(mySet);
+        // This will not be added because it is already contained in the set
+        mySet.add(23);
+        System.out.println();
+        printHashSet(mySet);
+        System.out.println();
+        mySet.remove(67);
+        printHashSet(mySet);
+        System.out.println("Does mySet contain 23: " +  mySet.contains(23));
+        System.out.println("Number of elements in mySet: " + mySet.size());
+    }
+    private static void printHashSet(Set<Integer> mySet) {
+        for (Integer i : mySet) {
+            System.out.println(i);
+        }
     }
 }
