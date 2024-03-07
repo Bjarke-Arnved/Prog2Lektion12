cf024eda8fba20974aa689595f9feb477d64b805
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-03-07 11:14:46 +0100

    Finished Exercise 2

--- src/opgave02/models/RangeIterator.java
+++ src/opgave02/models/RangeIterator.java
@@ -3,13 +3,28 @@ package opgave02.models;
 import java.util.Iterator;
 
 public class RangeIterator implements Iterator<Integer> {
+    private int from;
+    private int to;
+    private int current;
+    public RangeIterator(int from, int to) {
+        this.from = from;
+        this.to = to;
+        this.current = from;
+    }
     @Override
     public boolean hasNext() {
-        return false;
+        boolean hasNext;
+        if(current <= to) {
+            hasNext = true;
+        }
+        else {
+            hasNext = false;
+        }
+        return hasNext;
     }
 
     @Override
     public Integer next() {
-        return null;
+        return current++;
     }
 }
